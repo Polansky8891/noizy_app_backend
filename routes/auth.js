50be1d833ee52c5be807cf809aa2d8bc15b7a593
Pol Español Roca <pol8891@gmail.com>
2025-07-15 12:12:37 +0200

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+/*
+    Rutas de auth
+    host + /api/auth
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const router = Router();
+
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
+const { fieldsValidator } = require('../middlewares/fields-validators');
+
+
+router.post(
+    '/new',
+    [ // middlewares
+        check('name', 'name is mandatory').not().isEmpty(),
+        check('email', 'email is mandatory').isEmail(),
+        check('password', 'password must contain at least 6 characters').isLength({ min:6 }),
+        fieldsValidator
+    ],
+    createUser);
+
+router.post(
+    '/',
+    [ // middlewares
+        check('email', 'email is mandatory').isEmail(),
+        check('password', 'password must contain at least 6 characters').isLength({ min:6 }),
+        fieldsValidator
+    ],
+    loginUser);
+
+router.get('/renew', renewToken);
+
+
+module.exports = router;
+
