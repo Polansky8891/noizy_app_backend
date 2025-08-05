b0b5ba61c9208b2c348b4ef9652d5028b7b385e1
Pol Español Roca <pol8891@gmail.com>
2025-08-05 17:15:43 +0200

    cloudinary, musicUpload

---
+++ models/Track.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const TrackSchema = Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    artist: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        enum: ['Techno', 'Rock', 'Hip Hop', 'Pop', 'Electro', 'Jazz', 'Blues', 'Classic'],
+        default: 'Other',
+    },
+    audioUrl: {
+        type: String,
+        default: '',
+    },
+    coverUrl: {
+        type: String,
+        default: '',
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    plays: {
+        type: Number,
+        default: 0,
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+});
+
+module.exports = model('Track', TrackSchema );
+
